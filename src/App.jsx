f3bd0b97dc40ca90fba3195c6e69ae86ccc75cda
Simant Gurung <simant.xdezo@gmail.com>
2023-10-05 22:19:29 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+
+    <div>
+      <h1>Hello World</h1>
+      <h3 style={{color: "green"}}>I am a learner</h3>
+      <h4>Age : {10+13}</h4>
+    </div>
+  )
+}
+
+export default App
